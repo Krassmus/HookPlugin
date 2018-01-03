740b53a134b7054b4955af603f7de9253a5d1e2d
Rasmus Fuhse <fuhse@data-quest.de>
2018-01-03 17:50:21 +0100

    fix IfMessageHook

--- lib/IfHook.interface.php
+++ lib/IfHook.interface.php
@@ -2,16 +2,46 @@
 
 interface IfHook {
 
+    /**
+     * Return the name of the IfHook.
+     * @return string : a short name
+     */
     static public function getName();
 
     public function getParameters();
 
+    /**
+     * Returns an array of NotificationCenter events like 'CourseDidStore'. Return empty array if you don't want to
+     * listen to NotificationCenter.
+     * @return array
+     */
     public function listenToNotificationEvents();
 
+    /**
+     * We hand over the object (possibly a SORM) which we got from the NotificationCenter (if you dont listen
+     * to NotificationCenter, then ignore this method) and want to get all relevant Hook-objects.
+     * @param mixed $object : the $object we get from NotificationCenter
+     * @return array of Hook-objects
+     */
     public function findHooksByObject($object);
 
+    /**
+     * Returns a Flexi_Template for editing a Hook. The Hook object is given, although it might even be a new Hook
+     * object. Use the value of the Hook object to display the default values of your form.
+     * @param Hook $hook
+     * @return Flexi_Template
+     */
     public function getEditTemplate(Hook $hook);
 
+    /**
+     * Gets a hook, checks if it is responsible and returns an associative array with values to the parameters.
+     * These parameters will be given to the ThenHook afterwards.
+     * @param Hook $hook
+     * @param $type
+     * @param $event
+     * @param $object
+     * @return mixed
+     */
     public function check(Hook $hook, $type, $event, $object);
 
 }
--- lib/ThenHook.interface.php
+++ lib/ThenHook.interface.php
@@ -2,9 +2,26 @@
 
 interface ThenHook {
 
+    /**
+     * Return the name of the ThenHook.
+     * @return string : a short name
+     */
     static public function getName();
 
+    /**
+     * Returns a Flexi_Template for editing a Hook. The Hook object is given, although it might even be a new Hook
+     * object. Use the value of the Hook object to display the default values of your form.
+     * @param Hook $hook
+     * @return Flexi_Template
+     */
     public function getEditTemplate(Hook $hook, $attributes);
 
+    /**
+     * This method is now the real Hook. It sends emails, pushes webhooks, writes something or fetches data from the
+     * internet. Use the values of the parameters and the $hook['then_settings'] to fully customize the action of the ThenHook.
+     * @param Hook $hook
+     * @param array $parameters
+     * @return null|string : write something to the log. Do this if performing was successful. If it was unsuccessful it is recommended to throw an Exception with fitting message.
+     */
     public function perform(Hook $hook, $parameters);
 }
