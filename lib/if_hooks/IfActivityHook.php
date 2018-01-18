e2cc80feb5df78d2d58879b354a76da382758265
Rasmus Fuhse <fuhse@data-quest.de>
2018-01-18 11:01:43 +0100

    fix activities

--- lib/if_hooks/IfActivityHook.php
+++ lib/if_hooks/IfActivityHook.php
@@ -63,21 +63,65 @@ class IfActivityHook implements IfHook {
     public function check(Hook $hook, $type, $event, $activity)
     {
         if (($hook['user_id'] !== $activity['actor_id']) || !$hook['if_settings']['notmine']) {
-            $activity_array = $activity->toArray();
-            $url = "";
-            foreach ($activity['object_url'] as $u => $d) {
-                $url = $u;
-                break;
+            $provider = new $activity['provider']();
+            $allowed = $provider->getActivityDetails($activity);
+
+            if ($allowed) {
+                $url = "";
+                foreach ($activity['object_url'] as $u => $d) {
+                    $url = $u;
+                    if ($url) {
+                        break;
+                    }
+                }
+
+                $title = '';
+                $object_text = $provider::getLexicalField();
+                if (in_array($activity->actor_id, array('____%system%____', 'system')) !== false) {
+                    $actor = _('Stud.IP');
+                } else {
+                    $actor = get_fullname($activity->actor_id);
+                }
+                $contextclass = "Studip\\Activity\\".ucfirst($activity->context) . 'Context';
+                $contextcontainerclass = "Studip\\Activity\\".ucfirst($activity->context);
+                $context = $contextcontainerclass === "Studip\\Activity\\System"
+                    ? new $contextclass($provider)
+                    : new $contextclass(new $contextcontainerclass($activity->context_id), $provider);
+                $context_name = $context->getContextFullname();
+                switch ($activity->context) {
+                    case 'course':
+                        $title = $actor .' '
+                            . sprintf($activity->verbToText(),
+                                $object_text . sprintf(_(' im Kurs "%s"'), $context_name)
+                            );
+                        break;
+                    case 'institute':
+                        $title = $actor .' '
+                            . sprintf($activity->verbToText(),
+                                $object_text . sprintf(_(' in der Einrichtung "%s"'), $context_name)
+                            );
+                        break;
+                    case 'system':
+                        $title = $actor .' '
+                            . sprintf($activity->verbToText(), _('allen')) .' '
+                            . $object_text;
+                        break;
+                    case 'user':
+                        $title = $actor .' '
+                            . sprintf($activity->verbToText(), $context_name) .' '
+                            . $object_text;
+                        break;
+                }
+
+                return array(
+                    'text' => $title,
+                    'verb' => $activity["verb"],
+                    'user_id' => $activity["actor_id"],
+                    'name' => User::find($activity["actor_id"])->getFullName(),
+                    'url' => $url
+                );
             }
-            return array(
-                'text' => $activity_array["content"],
-                'verb' => $activity["verb"],
-                'user_id' => $activity["actor_id"],
-                'name' => User::find($activity["actor_id"])->getFullName(),
-                'url' => $url
-            );
-        } else {
-            return false;
         }
+        return false;
     }
 }
